625561b38b68315e0368664b564533d063ee83c8
GUMA GUMISIRIZA STANLEY <gumagumisiriza2001@gmail.com>
2025-01-27 07:23:37 +0300

    Checking voter eligibility

---
+++ Conditional.java
@@ -0,0 +1,12 @@
+public class Conditional {
+    public static void main(String[] args){
+        //finfing out if a person is eligible to vote;
+        int votingAge=18;
+        int myAge=23;
+        if(myAge>=votingAge){
+            System.out.println("Eligible to vote");
+        }else{
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
